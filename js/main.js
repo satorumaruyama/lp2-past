784c139dfb76d631247718fa4922ce8c2773b2ce
satorumaruyama <satolale77@gmail.com>
2024-06-14 14:55:06 +0900

    6/14

--- js/main.js
+++ js/main.js
@@ -1,23 +1,59 @@
 // カルーセル
-new Swiper('.swiper', {
-    slidesPerView: 'auto',
-    spaceBetween: 35,
-    breakpoints: {
-        390: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 3,
-      },
-    },
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+// new Swiper('.swiper', {
+//     slidesPerView: 'auto',
+//     spaceBetween: 35,
+//     breakpoints: {
+//         390: {
+//             slidesPerView: 1,
+//         },
+//         768: {
+//             slidesPerView: 3,
+//       },
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//   });
+$('.voice__slider').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  variableWidth: true,
+  responsive: [
+    // {
+    //   breakpoint: 1500,
+    //   settings: {
+    //     slidesToShow: 3,
+    //     slidesToScroll: 3,
+    //     // infinite: true,
+    //     dots: true
+    //   }
+    // },
+    // {
+    //   breakpoint: 600,
+    //   settings: {
+    //     slidesToShow: 2,
+    //     slidesToScroll: 2
+    //   }
+    // },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
 // アコーディオン
 $('.faq__answer').hide();
@@ -32,3 +68,4 @@ $('.faq__question').on('click', (e) => {
     }
     console.log("hello")
   });
+
